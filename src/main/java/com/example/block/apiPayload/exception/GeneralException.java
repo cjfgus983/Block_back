4757371b4a5a9e814a11fb921460a4600309c9a7
hyungwoo-choi <tinbed122@gmail.com>
2024-07-25 12:00:36 +0900

    설정

---
+++ src/main/java/com/example/block/apiPayload/exception/GeneralException.java
@@ -0,0 +1,18 @@
+package com.example.block.apiPayload.exception;
+
+import com.example.block.apiPayload.code.BaseErrorCode;
+import com.example.block.apiPayload.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+    private BaseErrorCode code;
+    public ErrorReasonDTO getErrorReason(){
+        return this.code.getReason();
+    }
+    public ErrorReasonDTO getErrorReasonHttpStatus(){
+        return this.code.getReasonHttpStatus();
+    }
+}
